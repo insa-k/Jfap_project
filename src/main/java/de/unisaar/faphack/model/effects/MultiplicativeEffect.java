b95c54b1e113ded249e627c199b727f4198c136f
Const4ntin3 <>
2019-07-23 14:00:08 +0200

    define CharacterModifier for MultiplicativeEffect

--- src/main/java/de/unisaar/faphack/model/effects/MultiplicativeEffect.java
+++ src/main/java/de/unisaar/faphack/model/effects/MultiplicativeEffect.java
@@ -26,7 +26,10 @@ public class MultiplicativeEffect extends ModifyingEffect {
    */
   public CharacterModifier apply(CharacterModifier c) {
     // TODO: FILL THIS
-    return null;
+    c.health = (int) (c.health*health);
+    c.magic = (int) (c.magic*magic);
+    c.power = (int) (c.power*power);
+    return c;
   }
 
 }
