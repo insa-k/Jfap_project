856c1a3aa72487222eef52de532b53b74b321c5a
Insa Kroeger <24524940+insa-k@users.noreply.github.com>
2019-07-25 15:13:44 +0200

    Tiles: check which character is on tile

--- src/main/java/de/unisaar/faphack/model/map/WallTile.java
+++ src/main/java/de/unisaar/faphack/model/map/WallTile.java
@@ -3,6 +3,8 @@ package de.unisaar.faphack.model.map;
 import de.unisaar.faphack.model.Character;
 import de.unisaar.faphack.model.MarshallingContext;
 
+import java.util.List;
+
 /**
  * Walltiles are used to determine the arrangement of a room. They usually
  * define the outer borders of a room, but might also be used within a room to
@@ -30,6 +32,10 @@ public class WallTile extends Tile {
     trait = WALL;
   }
 
+  public int getDestructible() {
+    return destructible;
+  }
+
   @Override
   public Tile willTake(Character c) {
     // TODO please implement me! (done)
@@ -54,5 +60,34 @@ public class WallTile extends Tile {
     destructible = c.readInt("destructible");
   }
 
+  /**
+   *
+   * @return true if the tile is occupied by a character
+   */
+  @Override
+  public boolean isOccupied(Character currentCharacter){
+    List<Character> inhabitants = room.getInhabitants();
+    for (Character c : inhabitants) {
+      Tile tile = c.getTile();
+      // if current character is on tile
+      if (this == tile && !(c == currentCharacter)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public Character characterOnTile() {
+    List<Character> inhabitants = room.getInhabitants();
+    for (Character c : inhabitants) {
+      Tile tile = c.getTile();
+      if (this == tile) {
+        return c;
+      }
+    }
+    // no character on tile
+    return null;
+  }
 
 }
