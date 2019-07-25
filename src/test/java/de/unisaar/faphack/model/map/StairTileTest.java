390b9a20bf3b14017a20d298167028f407fa276e
Const4ntin3 <>
2019-07-25 15:00:39 +0200

    add test to check if trap performs damage on character

--- src/test/java/de/unisaar/faphack/model/map/StairTileTest.java
+++ src/test/java/de/unisaar/faphack/model/map/StairTileTest.java
@@ -1,6 +1,8 @@
 package de.unisaar.faphack.model.map;
 
 import de.unisaar.faphack.model.Character;
+import de.unisaar.faphack.model.CharacterModifier;
+import de.unisaar.faphack.model.TestUtils;
 import org.junit.jupiter.api.Test;
 
 import static de.unisaar.faphack.model.TestUtils.*;
@@ -19,5 +21,34 @@ class StairTileTest {
     connectStairTiles(t1, t2, true);
     assertEquals(t2, t1.willTake(character));
     assertNull(t2.willTake(character));
+  }
+
+  @Test
+  void enterTrap(){
+    Room r1 = createSimpleRoom(8,8, 2);
+    Room r2 = createSimpleRoom(8, 8, 2);
+    StairTile st1 = (StairTile) r1.getTiles()[4][4];
+    StairTile st2 = (StairTile) r2.getTiles()[4][4];
+    connectStairTiles(st1, st2, true);
+
+    Trap trap = new Trap();
+    placeTrapOnTile(trap, st1);
+    trap.trapDoor = st1;
+    st1.trap = trap;
+    CharacterModifier characterModifier = TestUtils.createCharacterModifier(-10, -15, -1, 1);
+
+    trap.setEffect(characterModifier);
+
+    Character character = createBaseCharacter("Foo", 3,3);
+    addCharacter(r1, 4,4,character);
+    trap.applyTrapeffect(character);
+
+    trap.applyTrapeffect(character);
+    assertEquals(90, character.getHealth());
+    assertEquals(st2, st1.willTake(character));
+    assertNull(st2.willTake(character));
+
+
+
   }
 }
